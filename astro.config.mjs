124d4e06e8c9583e70840e541f9c4bffd3536457
Anggun Caksono <caksonoanggun@gmail.com>
2024-03-30 21:13:47 +0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import solidJs from "@astrojs/solid-js"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
+  site: "https://cakson.my.id",
+  base: "porto",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
   }), react()]
-});
+})
