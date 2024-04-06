6713000e1f47d75137e2f9a9e682ef1590a2eeaa
Anggun Caksono <caksonoanggun@gmail.com>
2024-04-06 10:34:21 +0700

    Update image paths in About.astro and data.js

--- src/data.js
+++ src/data.js
@@ -2,47 +2,47 @@ export const cardData = [
     {
         title: "Cloud Computing",
         items: [
-            { name: "Google Cloud Platform", image: "../public/skills/gcp.svg", alt: "GCP logo" },
-            { name: "Amazon Web Services", image: "../public/skills/aws.svg", alt: "AWS logo" },
+            { name: "Google Cloud Platform", image: "skills/gcp.svg", alt: "GCP logo" },
+            { name: "Amazon Web Services", image: "skills/aws.svg", alt: "AWS logo" },
             // Tambahkan data lainnya jika diperlukan
         ]
     },
     {
         title: "Programming",
         items: [
-            { name: "JavaScript", image: "../public/skills/javascript.svg", alt: "JavaScript logo" },
-            { name: "PHP", image: "../public/skills/php.svg", alt: "PHP logo" },
-            { name: "HTML", image: "../public/skills/html.svg", alt: "HTML logo" },
-            { name: "CSS", image: "../public/skills/css.svg", alt: "CSS logo" },
+            { name: "JavaScript", image: "skills/javascript.svg", alt: "JavaScript logo" },
+            { name: "PHP", image: "skills/php.svg", alt: "PHP logo" },
+            { name: "HTML", image: "skills/html.svg", alt: "HTML logo" },
+            { name: "CSS", image: "skills/css.svg", alt: "CSS logo" },
             // Tambahkan bahasa pemrograman lainnya jika diperlukan
         ]
     },
     {
         title: "Framework",
         items: [
-            { name: "Vite JS", image: "../public/skills/vite.svg", alt: "Vite logo" },
-            { name: "Astro", image: "../public/skills/astro.svg", alt: "Astro logo" },
-            { name: "Next JS", image: "../public/skills/next.svg", alt: "Next logo" },
-            { name: "Tailwind CSS", image: "../public/skills/tailwind.svg", alt: "Tailwind logo" },
-            { name: "Bootstrap", image: "../public/skills/bootstrap.svg", alt: "Bootstrap logo" },
+            { name: "Vite JS", image: "skills/vite.svg", alt: "Vite logo" },
+            { name: "Astro", image: "skills/astro.svg", alt: "Astro logo" },
+            { name: "Next JS", image: "skills/next.svg", alt: "Next logo" },
+            { name: "Tailwind CSS", image: "skills/tailwind.svg", alt: "Tailwind logo" },
+            { name: "Bootstrap", image: "skills/bootstrap.svg", alt: "Bootstrap logo" },
             // Tambahkan bahasa pemrograman lainnya jika diperlukan
         ]
     },
     {
         title: "Database",
         items: [
-            { name: "MySQL", image: "../public/skills/mysql.svg", alt: "MySQL logo" },
-            { name: "MongoDB", image: "../public/skills/mongo.svg", alt: "MongoDB logo" },
-            { name: "Firebase", image: "../public/skills/firebase.svg", alt: "Firebase logo" },
+            { name: "MySQL", image: "skills/mysql.svg", alt: "MySQL logo" },
+            { name: "MongoDB", image: "skills/mongo.svg", alt: "MongoDB logo" },
+            { name: "Firebase", image: "skills/firebase.svg", alt: "Firebase logo" },
             // Tambahkan bahasa pemrograman lainnya jika diperlukan
         ]
     },
     {
         title: "Design | UI/UX",
         items: [
-            { name: "Figma", image: "../public/skills/figma.svg", alt: "Figma logo" },
-            { name: "Lunacy", image: "../public/skills/lunacy.svg", alt: "Lunacy logo" },
-            { name: "Adobe XD", image: "../public/skills/xd.svg", alt: "Adobe XD logo" },
+            { name: "Figma", image: "skills/figma.svg", alt: "Figma logo" },
+            { name: "Lunacy", image: "skills/lunacy.svg", alt: "Lunacy logo" },
+            { name: "Adobe XD", image: "skills/xd.svg", alt: "Adobe XD logo" },
             // Tambahkan bahasa pemrograman lainnya jika diperlukan
         ]
     },
