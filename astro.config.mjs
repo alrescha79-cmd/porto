d9e90e51a6ffa6cfe242dc365a670af2492cb115
Anggun Caksono <caksonoanggun@gmail.com>
2024-04-15 11:20:23 +0700

    Update Astro configuration and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,11 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 import react from "@astrojs/react"
-import vercel from "@astrojs/vercel/serverless"
 
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cakson.my.id",
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   integrations: [mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
   }), react()],
