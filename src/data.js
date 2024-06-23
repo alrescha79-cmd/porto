3031557a7e26927a5f7e8a1cf5cb3785adb9266d
Anggun Caksono <caksonoanggun@gmail.com>
2024-06-23 22:19:17 +0700

    chore: Update certificates page

--- src/data.js
+++ src/data.js
@@ -1,20 +1,12 @@
 export const cardData = [
     {
-        title: "Cloud Computing",
-        items: [
-            { name: "Google Cloud Platform", image: "skills/gcp.svg", alt: "GCP logo" },
-            { name: "Amazon Web Services", image: "skills/aws.svg", alt: "AWS logo" },
-            // Tambahkan data lainnya jika diperlukan
-        ]
-    },
-    {
         title: "Programming",
         items: [
             { name: "JavaScript", image: "skills/javascript.svg", alt: "JavaScript logo" },
+            { name: "Kotlin", image: "skills/kotlin.svg", alt: "kotlin logo" },
             { name: "PHP", image: "skills/php.svg", alt: "PHP logo" },
             { name: "HTML", image: "skills/html.svg", alt: "HTML logo" },
             { name: "CSS", image: "skills/css.svg", alt: "CSS logo" },
-            // Tambahkan bahasa pemrograman lainnya jika diperlukan
         ]
     },
     {
@@ -25,7 +17,7 @@ export const cardData = [
             { name: "Next JS", image: "skills/next.svg", alt: "Next logo" },
             { name: "Tailwind CSS", image: "skills/tailwind.svg", alt: "Tailwind logo" },
             { name: "Bootstrap", image: "skills/bootstrap.svg", alt: "Bootstrap logo" },
-            // Tambahkan bahasa pemrograman lainnya jika diperlukan
+            { name: "Express", image: "skills/express.svg", alt: "Express logo" },
         ]
     },
     {
@@ -34,7 +26,14 @@ export const cardData = [
             { name: "MySQL", image: "skills/mysql.svg", alt: "MySQL logo" },
             { name: "MongoDB", image: "skills/mongo.svg", alt: "MongoDB logo" },
             { name: "Firebase", image: "skills/firebase.svg", alt: "Firebase logo" },
-            // Tambahkan bahasa pemrograman lainnya jika diperlukan
+            { name: "Supabase", image: "skills/supabase.svg", alt: "Supabase logo" },
+        ]
+    },
+    {
+        title: "Cloud Computing",
+        items: [
+            { name: "Google Cloud Platform", image: "skills/gcp.svg", alt: "GCP logo" },
+            { name: "Amazon Web Services", image: "skills/aws.svg", alt: "AWS logo" },
         ]
     },
     {
@@ -43,8 +42,6 @@ export const cardData = [
             { name: "Figma", image: "skills/figma.svg", alt: "Figma logo" },
             { name: "Lunacy", image: "skills/lunacy.svg", alt: "Lunacy logo" },
             { name: "Adobe XD", image: "skills/xd.svg", alt: "Adobe XD logo" },
-            // Tambahkan bahasa pemrograman lainnya jika diperlukan
         ]
     },
-    // Tambahkan judul dan data kartu lainnya jika diperlukan
 ]
